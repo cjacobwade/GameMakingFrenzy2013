95753cf40701e1ea36324bd36be2079c7d418304
Christopher Wade <cjacobwade@gmail.com>
2013-09-14 12:00:24 -0500

    Robot and button controls

--- GameMakingFrenzy2013/Assets/Scripts/Robot.cs
+++ GameMakingFrenzy2013/Assets/Scripts/Robot.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Cube : MonoBehaviour {
+public class Robot : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
---
+++ GameMakingFrenzy2013/Assets/Scripts/UIController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIController : MonoBehaviour {
+	
+	Quaternion gyroRot;
+	Transform buttonHeld;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		UIControl();
+	}
+	
+	void UIControl()
+	{
+		Ray touchPos = Camera.main.ScreenPointToRay(Input.mousePosition);
+		RaycastHit hit;
+		if(Input.GetMouseButtonDown(0))
+			
+			if(Physics.Raycast(touchPos,out hit))
+			{
+				if(hit.transform.CompareTag("Button"))
+				{
+					buttonHeld = hit.transform;
+					hit.transform.SendMessage("Click",SendMessageOptions.DontRequireReceiver);
+				}
+			}
+		if(Input.GetMouseButtonUp(0) && buttonHeld)
+		{
+			buttonHeld.SendMessage("Release", SendMessageOptions.DontRequireReceiver);
+			buttonHeld = null;
+		}
+	}
+}
